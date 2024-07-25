a55e4f8aca0f1598c76156314ad50c39e9324605
blerafon12 <blerafon12@gmail.com>
2024-07-25 10:07:17 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -31,8 +31,9 @@ with t1 as (
     select distinct
         s.customer_id,
         to_char(s.sale_date, 'YYYY-MM') as selling_month,
-        floor(sum(s.quantity * p.price)
-            over (partition by date_trunc('month', s.sale_date))
+        floor(
+            sum(s.quantity * p.price)
+                over (partition by date_trunc('month', s.sale_date))
         ) as income
     from sales s
     inner join products p on s.product_id = p.product_id
