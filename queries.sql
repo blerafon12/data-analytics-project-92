071b6e23b9b68f19ced882f00f937f4fbbb0994d
blerafon12 <blerafon12@gmail.com>
2024-07-25 09:35:35 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -23,14 +23,14 @@ select
 from t1
 group by age_category
 order by age_category;
- -------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --отчет по количеству уникальных покупателей и выручке, которую они принесли
 with t1 as (
 --приведение столбца sale_date к виду ГОД-МЕСЯЦ
 --подсчет суммы выручки за этот месяц округленной до целого
     select distinct
-        to_char(sale_date,'YYYY-MM') as selling_month,
         customer_id,
+        to_char(sale_date,'YYYY-MM') as selling_month,
         floor(sum(quantity*price) over(partition by date_trunc('month', sale_date))) as income
     from sales s
     inner join products p  on p.product_id = s.product_id
