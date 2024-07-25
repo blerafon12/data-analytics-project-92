3e8bca5b3554d1fee2eb46b4daffeb71d272965c
blerafon12 <blerafon12@gmail.com>
2024-07-25 09:18:38 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -4,14 +4,14 @@ select count(customer_id) as custamers_count from customers;
 --количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
 with t1 as (
 --запрос для распределения возрастных груп относительно столбца age 
-	select
-    	age,
-		case 
-			when age between 16 and 25 then '16-25'
-			when age between 26 and 40 then '26-40'
-			when age > 40 then '40+'
-		end as age_category
-	from customers
+select
+age,
+case 
+when age between 16 and 25 then '16-25'
+when age between 26 and 40 then '26-40'
+when age > 40 then '40+'
+end as age_category
+from customers
 )
 --запрос на получение итогового результата
 --определение выборки с уникальными значениями 
